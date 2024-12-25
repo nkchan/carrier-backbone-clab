a314868ea7690ef48996c90f1583f83978d8a252
Kaoru Nagashima <nkchan0228@gmail.com>
2024-12-25 20:36:15 +0900

    add ospf

--- save.sh
+++ save.sh
@@ -1,4 +1,4 @@
 #! /usr/bin/bash 
 ssh clab-carrier-backbone-bb "show configuration | display set" > config/set_style/bb.config
-ssh clab-carrier-backbone-bb "show configuration" > config/bb.config
+ssh clab-carrier-backbone-bb "show configuration" > clab-carrier-backbone/bb/config/startup-config.cfg
 
